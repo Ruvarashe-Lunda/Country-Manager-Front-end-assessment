8e8ba2a4d4f1c5cf2959b6ff68b00f00881bb612
Ruvarashe-Lunda <primrose@velocityinc.tech>
2023-07-05 16:06:53 +0200

    Login update

--- Login.js
+++ Login.js
@@ -33,3 +33,56 @@ const Login = () => {
 };
 
 export default Login;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    const loginButton = document.getElementById("login-btn");
+    const menuTab = document.getElementById("menu-tab");
+    const logoutButton = document.getElementById("logout-btn");
+  
+    // Enable/disable login button based on input validity
+    function updateLoginButton() {
+      const username = usernameInput.value;
+      const password = passwordInput.value;
+  
+      const isValidUsername = username.trim().length > 0;
+      const isValidPassword =
+        password.length >= 8 &&
+        /[A-Z]/.test(password) &&
+        /\d/.test(password) &&
+        /[^a-zA-Z0-9]/.test(password);
+  
+      loginButton.disabled = !(isValidUsername && isValidPassword);
+    }
+  
+    // Handle login button click
+    loginButton.addEventListener("click", function () {
+      const username = usernameInput.value;
+      const password = passwordInput.value;
+  
+      // Perform login validation here
+      if (/* perform your login validation */) {
+        // Successful login, show menu tab and hide login form
+        menuTab.style.display = "block";
+        document.getElementById("login-form").style.display = "none";
+      } else {
+        alert("Invalid username or password. Please try again.");
+      }
+    });
+  
+    // Handle logout button click
+    logoutButton.addEventListener("click", function () {
+      // Clear input fields
+      usernameInput.value = "";
+      passwordInput.value = "";
+  
+      // Hide menu tab and show login form
+      menuTab.style.display = "none";
+      document.getElementById("login-form").style.display = "block";
+    });
+  
+    // Add input event listeners to check input validity on every keystroke
+    usernameInput.addEventListener("input", updateLoginButton);
+    passwordInput.addEventListener("input", updateLoginButton);
+  });
